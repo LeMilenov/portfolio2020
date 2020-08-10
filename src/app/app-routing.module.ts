7da47d29af33d1ba897fa0fc5752439f26cb6d36
Daniel Milenov Milanov <dmilanov@aqtr.com>
2020-08-09 23:05:33 -0400

    basic home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectComponent } from './projects/project/project.component';
-
+/**
+ * 
+ * scrollPositionRestoration: on change of router url the position of the screen will set to the top.
+   anchorScrolling : When set to ‘enabled’, scrolls to the anchor element when the URL has a fragment. Anchor scrolling is disabled by default.
+   scrollOffset: Configures the scroll offset the router will use when scrolling to an element.
+   source : https://www.geekstrick.com/fragment-url-in-angular-8/#Router_Options
+ */
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+  scrollOffset: [0, 64],
+};
 const routes: Routes = [
   {path: 'About', component: AboutComponent},
   {path: 'Projects', component: ProjectsComponent},
   {path: 'Projects/:id', component: ProjectComponent},
   {path: '', component: HomeComponent},
   {path: '**', redirectTo: '', pathMatch: 'full'},
-
-
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  ids: Array<String> = ['main','about','projects']
   constructor() { }
 
   ngOnInit() {
