13fb0d236e8f546cad2c95efdd0af2642729b83f
Daniel Milenov Milanov <dmilanov@aqtr.com>
2020-08-14 00:40:16 -0400

    add big ball + change logo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,102 @@
 import { Component, OnInit } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  ids: Array<String> = ['main','about','projects']
-  constructor() { }
 
+export class HomeComponent implements OnInit {
+  constructor() { }
   ngOnInit() {
+  
+  }
+  ngAfterViewInit() :void {
+    this.sphereAnimation();
   }
 
+  
+  private sphereAnimation(){
+    var sphereEl = document.querySelector('.sphere-animation');
+    var spherePathEls = sphereEl.querySelectorAll('.sphere path');
+    var pathLength = spherePathEls.length;
+    var hasStarted = false;
+    var aimations = [];
+    var accentMain = "rgb(29, 179, 109)";
+    var accentSecondary = "rgb(206, 206, 206)";
+    this.fitElementToParent(sphereEl);
+
+    var breathAnimation = anime({
+      begin: function() {
+        for (var i = 0; i < pathLength; i++) {
+          aimations.push(anime({
+            targets: spherePathEls[i],
+            stroke: {value: [accentSecondary, accentMain], duration: 500},
+            translateX: [2, -4],
+            translateY: [2, -4],
+            easing: 'easeOutQuad',
+            autoplay: false
+          }));
+        }
+      },
+      update: function(ins) {
+        aimations.forEach(function(animation, i) {
+          var percent = (1 - Math.sin((i * .35) + (.0022 * ins.currentTime))) / 2;
+          animation.seek(animation.duration * percent);
+        });
+      },
+      duration: Infinity,
+      autoplay: false
+    });
+
+    var introAnimation = anime.timeline({
+      autoplay: false
+    }).add({
+      targets: spherePathEls,
+      strokeDashoffset: {
+        value: [anime.setDashoffset, 0],
+        duration: 3900,
+        easing: 'easeInOutCirc',
+        delay: anime.stagger(190, {direction: 'reverse'})
+      },
+      duration: 2000,
+      delay: anime.stagger(60, {direction: 'reverse'}),
+      easing: 'linear'
+    }, 0);
+
+    var shadowAnimation = anime({
+      targets: '#sphereGradient',
+      x1: '25%',
+      x2: '25%',
+      y1: '0%',
+      y2: '75%',
+      duration: 30000,
+      easing: 'easeOutQuint',
+      autoplay: false
+    },0);
+
+    breathAnimation.play();
+    introAnimation.play();
+    shadowAnimation.play();
+  }
+
+  private fitElementToParent(el, padding = 0) {
+    var timeout = null;
+    function resize() {
+      if (timeout) clearTimeout(timeout);
+      
+      anime.set(el, {scale: 1});
+      var pad = padding || 0;
+      var parentEl = el.parentNode;
+      var elOffsetWidth = el.offsetWidth - pad;
+      var parentOffsetWidth = parentEl.offsetWidth;
+      var ratio = parentOffsetWidth / elOffsetWidth;
+      timeout = setTimeout(()=>{
+        anime.set(el, {scale: ratio})
+      }, 10);
+    }
+    resize();
+    window.addEventListener('resize', resize);
+  }
 }
