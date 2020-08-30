13cd4bc1bec276907ecfe4ac74d01f9adaad133f
Daniel Milenov Milanov <dmilanov@aqtr.com>
2020-08-29 22:32:25 -0400

    add color animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,20 +21,56 @@ export class HomeComponent implements OnInit {
     'pawel-czerwinski-rsB6JZg6rVs-unsplash.jpg',
     'pawel-czerwinski-rsB6JZg6rVs-unsplash.jpg'
   ]
-  private TIME_TO_WAIT_BETWEEN_ANIMATIONS = 5000;
+  private TIME_TO_WAIT_BETWEEN_ANIMATIONS = 8000;
   private selectedSvg:number = -1;
   public backgroundName = this.backgrounds[0];
   constructor() {}
   ngOnInit() {}
 
   ngAfterViewInit() {
-    this.changeToRandomPath();
+    this.startLoop();
   }
-  /**
-   * changes the current path every 15 secs + changes background
-   */
-  private changeToRandomPath() {
-    //get random value
+
+  private startLoop(){
+
+    this.changePath();
+    this.changeBackground();
+    var animation = this.createAnimation();
+    animation.play();
+
+    setTimeout(()=>{
+      this.startLoop();
+    },this.TIME_TO_WAIT_BETWEEN_ANIMATIONS);
+  }
+  private createAnimation():any{
+    var animation = anime.timeline({
+      easing:'linear',
+      autoplay:false
+    });
+    //fade in background
+    animation.add({
+      targets: "#background",
+      opacity: 1,
+      duration: 1000,
+          
+    })
+    //change path
+    .add({
+      duration: 5000,
+      targets: "#path-to-animate", 
+      d: this.svg[this.selectedSvg],
+    })
+    //fade background
+    .add({
+      duration: 1000,
+      targets: "#background",
+      opacity: 0, 
+    });
+    return animation;
+
+  }
+
+  private changePath(){
     var random = Math.floor(Math.random() * (this.svg.length));
     // if its the same as the selected, we keep searching
     while(random == this.selectedSvg){
@@ -42,174 +78,13 @@ export class HomeComponent implements OnInit {
     }
     //we set it as the new value
     this.selectedSvg = random;
-    //load new path
-    var currentAnimation = anime({
-      targets: "#path-to-animate", 
-      d: [
-        { value: this.svg[this.selectedSvg] }, 
-      ], 
-      easing: "linear", 
-      duration: 5000, 
-      autoplay: true, 
-    });
-    //load new background 
-    random = Math.floor(Math.random() * (this.backgrounds.length));
-    var fadeAnimation= anime({
-      targets: "#background",
-      opacity: [{value:1},{value:0}],
-      duration:1000,
-      complete: function(){ 
-        
-      }
-      
-    })
-    // give new background to newBackgroundName
-    // fade to color -> white the current background 
-    // fade to white -> color the new background
-    //set the currentBackground = newBackground
-    //hide new Background
-    // currentAnimation.add({
-    //   targets:
-    // });
-    setTimeout(()=>{
-      this.changeToRandomPath();
-    },this.TIME_TO_WAIT_BETWEEN_ANIMATIONS);
   }
-  //   //we open the first disk
-  //   // this.openDisk(this.NUMBER_OF_DISKS_OFFSET);
-  // }
-  // /**
-  //  * creates the basic animations for the svg sphere
-  //  * use var objects for the begin / update :function()
-  //  */
-  // private initSphereAnimation(){
-  //   var sphereEl = this.sphereEl;
-  //   var spherePathEls = this.spherePathEls;
-  //   this.fitElementToParent(sphereEl);
-
-  //   this.initBreathAnimation(spherePathEls);
-  //   this.initIntroAnimation(spherePathEls);
-  //   this.initShadowAnimation();
-
-  //   this.playSphereAnimation();
-  // }
-  // /**
-  //  * create breathing animation
-  //  * @param spherePathEls
-  //  */
-  // private initBreathAnimation( spherePathEls: any[]){
-  //   var pathLength = this.pathLength;
-  //   var hasStarted = false;
-  //   var animations = [];
-  //   var accentMain = "rgb(29,     179,     109)";
-  //   var accentSecondary = "rgb(206,     206,     206)";
-  // //begin and update function need a local variable somehow ??
-  // this.breathAnimation = anime({
-  //   begin: function() {
-  //     for (var i = 0; i < pathLength; i++) {
-  //       //animate every path except the opendisk
-  //       animations.push(anime({
-  //         targets: spherePathEls[i], 
-  //         stroke: {value: [accentSecondary,     accentMain],     duration: 500}, 
-  //         translateX: [2,     -4], 
-  //         translateY: [2,     -4], 
-  //         easing: 'easeOutQuad', 
-  //         autoplay: false
-  //       }));
-  //     }
-  //   }, 
-  //   update: function(ins) {
-  //     animations.forEach(function(animation,     i) {
-  //       var percent = (1 - Math.sin((i * .35) + (.0022 * ins.currentTime))) / 2;
-  //       animation.seek(animation.duration * percent);
-  //     });
-  //   }, 
-  //   duration: Infinity, 
-  //   autoplay: false
-  // });
-  // }
-  // /**
-  //  * create introAnimation
-  //  * @param spherePathEls
-  //  */
-  // private initIntroAnimation( spherePathEls: any[]){
-  //   this.introAnimation = anime.timeline({
-  //     autoplay: false
-  //   }).add({
-  //     targets: spherePathEls, 
-  //     strokeDashoffset: {
-  //       value: [anime.setDashoffset,     0], 
-  //       duration: 3900, 
-  //       easing: 'easeInOutCirc', 
-  //       delay: anime.stagger(190,     {direction: 'reverse'})
-  //     }, 
-  //     duration: 2000, 
-  //     delay: anime.stagger(60,     {direction: 'reverse'}), 
-  //     easing: 'linear'
-  //   },     0);
-  // }
-  // /**
-  //  * create shadow animation
-  //  */
-  // private initShadowAnimation(){
-  //   this.shadowAnimation = anime({
-  //     targets: '#sphereGradient', 
-  //     x1: '25%', 
-  //     x2: '25%', 
-  //     y1: '0%', 
-  //     y2: '75%', 
-  //     duration: 30000, 
-  //     easing: 'easeOutQuint', 
-  //     autoplay: false
-  //   },    0);
-  // }
-  // /**
-  //  * play all animations
-  //  */
-  // private playSphereAnimation(){
-  //   if(this.breathAnimation && this.introAnimation && this.shadowAnimation){
-  //     this.breathAnimation.play();
-  //     this.introAnimation.play();
-  //     this.shadowAnimation.play();
-  //   }
-  // }
-  // private pauseSphereAnimation(){
-  //   if(this.breathAnimation && this.introAnimation && this.shadowAnimation){
-  //     this.breathAnimation.pause();
-  //     this.introAnimation.pause();
-  //     this.shadowAnimation.pause();
-  //   }
-  // }
-  // /**
-  //  * restart with new spherePathEls
-  //  */
-  // private restartAnimationWithNewValues(){
-  //   this.breathAnimation.pause();
-  //   this.initBreathAnimation(this.spherePathEls);
-  //   this.breathAnimation.play();
-  // }
-  // /**
-  //  * fit the sphere to the screen
-  //  * @param el
-  //  * @param padding
-  //  */
-  // private fitElementToParent(el,     padding = 0) {
-  //   var timeout = null;
-  //   function resize() {
-  //     if (timeout) clearTimeout(timeout);
-
-  //     anime.set(el,     {scale:1});
-  //     var pad = padding || 0;
-  //     var parentEl = el.parentNode;
-  //     var elOffsetWidth = el.offsetWidth - pad;
-  //     var parentOffsetWidth = parentEl.offsetWidth;
-  //     var ratio = parentOffsetWidth / elOffsetWidth;
-
-  //     timeout = setTimeout(()=>{
-  //       anime.set(el,     {scale: ratio})
-  //     },     10);
-  //   }
-  //   resize();
-  //   window.addEventListener('resize',     resize);
-  // }
+ 
+  private changeBackground(){
+     //load new background 
+    var random = Math.floor(Math.random() * (this.backgrounds.length));
+    this.backgroundName = this.backgrounds[random];   
+  }
+  
+  
 }
