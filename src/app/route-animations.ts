3dc4377807bec338df4b07dfada06ecfa73a8697
Daniel <dani_milenov@hotmail.fr>
2020-09-22 10:27:43 -0400

    change img size and optimize route animation code

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -9,37 +9,170 @@ import {
 
     export const slider =
       trigger('routeAnimations', [
-        transition('isRight => isFarRight', slideTo('right') ),
-        transition('isFarRight => isRight', slideTo('left') ),
-        transition('isRight => *', slideTo('left') ),
-        transition('isLeft => *', slideTo('right') ),
-        transition('* => isLeft', slideTo('left') ),
-        transition('* => isRight', slideTo('right') )
+        transition('isRight => isFarRight',  [
+          query(':enter, :leave', [
+            style({
+              position: 'relative',
+              top: 0,
+              opacity:0,
+              background:'red',
+              right: 0,
+              width: '50%',           
+            })
+          ]),
+          query(':enter', [
+            style({ right: '-100%'})
+          ]),
+          group([
+            query(':leave', [
+              animate('600ms ease-out', style({right: '100%' }))
+            ]),
+            query(':enter', [
+              animate('600ms ease-out', style({ right: '0%'}))
+            ])
+          ])       
+        ]),
+
+        transition('isFarRight => isRight', [
+          query(':enter, :leave', [
+            style({
+              position: 'relative',
+              top: 0,
+              opacity:0,
+              background:'red',
+              left: 0,
+              width: '50%',           
+            })
+          ]),
+          query(':enter', [
+            style({ left: '-100%'})
+          ]),
+          group([
+            query(':leave', [
+              animate('600ms ease-out', style({left: '100%' }))
+            ]),
+            query(':enter', [
+              animate('600ms ease-out', style({ left: '0%'}))
+            ])
+          ])       
+        ] ),
+        transition('isRight => *', [
+          query(':enter, :leave', [
+            style({
+              position: 'relative',
+              top: 0,
+              opacity:0,
+              background:'red',
+              left: 0,
+              width: '50%',           
+            })
+          ]),
+          query(':enter', [
+            style({ left: '-100%'})
+          ]),
+          group([
+            query(':leave', [
+              animate('600ms ease-out', style({left: '100%' }))
+            ]),
+            query(':enter', [
+              animate('600ms ease-out', style({ left: '0%'}))
+            ])
+          ])       
+        ]  ),
+        transition('isLeft => *', [
+          query(':enter, :leave', [
+            style({
+              position: 'relative',
+              top: 0,
+              opacity:0,
+              background:'red',
+              right: 0,
+              width: '50%',           
+            })
+          ]),
+          query(':enter', [
+            style({ right: '-100%'})
+          ]),
+          group([
+            query(':leave', [
+              animate('600ms ease-out', style({right: '100%' }))
+            ]),
+            query(':enter', [
+              animate('600ms ease-out', style({ right: '0%'}))
+            ])
+          ])       
+        ] ),
+        transition('* => isLeft', [
+          query(':enter, :leave', [
+            style({
+              position: 'relative',
+              top: 0,
+              opacity:0,
+              background:'red',
+              left: 0,
+              width: '50%',           
+            })
+          ]),
+          query(':enter', [
+            style({ left: '-100%'})
+          ]),
+          group([
+            query(':leave', [
+              animate('600ms ease-out', style({left: '100%' }))
+            ]),
+            query(':enter', [
+              animate('600ms ease-out', style({ left: '0%'}))
+            ])
+          ])       
+        ] ),
+        transition('* => isRight', [
+          query(':enter, :leave', [
+            style({
+              position: 'relative',
+              top: 0,
+              opacity:0,
+              background:'red',
+              right: 0,
+              width: '50%',           
+            })
+          ]),
+          query(':enter', [
+            style({ right: '-100%'})
+          ]),
+          group([
+            query(':leave', [
+              animate('600ms ease-out', style({right: '100%' }))
+            ]),
+            query(':enter', [
+              animate('600ms ease-out', style({ right: '0%'}))
+            ])
+          ])       
+        ] )
       ]);
     
-    function slideTo(direction:any) {
-      const optional = { optional: true };
-      return [
-        query(':enter, :leave', [
-          style({
-            position: 'relative',
-            top: 0,
-            opacity:0,
-            background:'red',
-            [direction]: 0,
-            width: '50%',           
-          })
-        ], optional),
-        query(':enter', [
-          style({ [direction]: '-100%'})
-        ]),
-        group([
-          query(':leave', [
-            animate('600ms ease-out', style({ [direction]: '100%' }))
-          ], optional),
-          query(':enter', [
-            animate('600ms ease-out', style({ [direction]: '0%'}))
-          ])
-        ])       
-      ];
-    }
+    // export function slideTo(direction:any) {
+    //   const optional = { optional: true };
+    //   return [
+    //     query(':enter, :leave', [
+    //       style({
+    //         position: 'relative',
+    //         top: 0,
+    //         opacity:0,
+    //         background:'red',
+    //         [direction]: 0,
+    //         width: '50%',           
+    //       })
+    //     ], optional),
+    //     query(':enter', [
+    //       style({ [direction]: '-100%'})
+    //     ]),
+    //     group([
+    //       query(':leave', [
+    //         animate('600ms ease-out', style({ [direction]: '100%' }))
+    //       ], optional),
+    //       query(':enter', [
+    //         animate('600ms ease-out', style({ [direction]: '0%'}))
+    //       ])
+    //     ])       
+    //   ];
+    // }
